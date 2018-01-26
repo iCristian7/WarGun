e7ea9abc5ea1a6021c60bdbaa6843044b1166579
Hideki96 <blakipirill.santos@gmail.com>
2018-01-26 16:32:57 +0100

    Que va casi todo

---
+++ Prueba Movimientos Personaje/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3 (target.transform.position.x, target.transform.position.y, -100);
+		
+	}
+}
